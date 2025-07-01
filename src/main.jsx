9d648d32a7c7cd20a52321411b4897503792a05f
chaitu347 <chaitu347347@gmail.com>
2025-07-01 19:45:33 +0530

    some error changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import Contactform from './contactform/Contactform.jsx'
+
 import Navbar from './contactform/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
